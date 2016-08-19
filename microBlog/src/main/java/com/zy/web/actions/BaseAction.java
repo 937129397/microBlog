86b5e8d68cc14977d0e515d11bbf9d4e1bbdacdf
zy <937129397@qq.com>
2016-08-19 21:12:21 +0800

    框架

---
+++ microBlog/src/main/java/com/zy/web/actions/BaseAction.java
@@ -0,0 +1,39 @@
+package com.zy.web.actions;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.opensymphony.xwork2.ActionSupport;
+import com.zy.web.model.JsonModel;
+
+public abstract class BaseAction extends ActionSupport {
+	private static final long serialVersionUID = 1L;
+	protected JsonModel jsonModel;
+
+	private String parseJson(JsonModel jsonModel) {
+		Gson g = new Gson();
+		return g.toJson(jsonModel);
+	}
+
+	public void printJson(JsonModel jsonModel, HttpServletResponse response)
+			throws IOException {
+		String json = parseJson(jsonModel);
+		// TODO budong
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setContentType("application/json;charset=utf-8");
+
+		PrintWriter out = response.getWriter();
+		out.println(json);
+		out.flush();
+		out.close();
+	}
+
+	@Resource(name = "jsonModel")
+	public void setJm(JsonModel jsonModel) {
+		this.jsonModel = jsonModel;
+	}
+}
