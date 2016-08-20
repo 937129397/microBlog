493093c0d08988e2e49d9a8108a3d61ae826124c
ZQB <719725611@qq.com>
2016-08-20 16:54:02 +0800

    发布消息

--- microBlog/src/test/java/test/Test.java
+++ microBlog/src/test/java/test/Test.java
@@ -1,13 +1,30 @@
 package test;
 
-import java.util.Date;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+
+
+import com.microblog.bean.Blog;
+import com.microblog.biz.BlogBiz;
 
 import junit.framework.TestCase;
 
 public class Test extends TestCase {
 
-	protected void runTest() {
-		Date d = new Date();
-				d.getTime();
+	
+	
+	public void testApp07() {
+		ApplicationContext ac = new ClassPathXmlApplicationContext(
+				"beans_mybatis.xml");
+		BlogBiz ub=(BlogBiz) ac.getBean("blogBizImpl");
+		Blog blog=new Blog();
+		blog.setUid("1");
+		blog.setText("你好");
+		blog.setSource(1);
+		blog.setPic("图片");
+		ub.saveBlog(blog);
 	}
 }
