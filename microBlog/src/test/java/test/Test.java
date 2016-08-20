47dda2ffb521fceabd5a7660ab76ed2d884bea3e
MapleIsMine <571880590@qq.com>
2016-08-20 17:47:50 +0800

    登陆实现

--- microBlog/src/test/java/test/Test.java
+++ microBlog/src/test/java/test/Test.java
@@ -1,16 +1,15 @@
 package test;
 
 
+import junit.framework.TestCase;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-
-
-
 import com.microblog.bean.Blog;
+import com.microblog.bean.User;
 import com.microblog.biz.BlogBiz;
-
-import junit.framework.TestCase;
+import com.microblog.biz.UserBiz;
 
 public class Test extends TestCase {
 
@@ -26,5 +25,19 @@ public class Test extends TestCase {
 		blog.setSource(1);
 		blog.setPic("图片");
 		ub.saveBlog(blog);
+	}
+	
+	public void logintest(){
+		ApplicationContext ac = new ClassPathXmlApplicationContext(
+				"beans_mybatis.xml");
+		UserBiz ub = (UserBiz) ac.getBean("userBizImpl");
+		
+		User user = new User();
+		
+		user.setEmail("571880590@qq.com");
+		user.setPassword("a");
+		ub.loginByEmail(user);
+		System.out.println("登陆成功");
+		
 	}
 }
