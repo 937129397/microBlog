522e0bed4b3afae80360a1e1807fbaa8e49e9fb5
MapleIsMine <571880590@qq.com>
2016-08-23 19:44:48 +0800

    用户分组的数据库准备

--- microBlog/src/main/resources/create.sql
+++ microBlog/src/main/resources/create.sql
@@ -12,11 +12,12 @@ create table users(
 	level int,--用户等级
 	exp int,--用户经验
 	regDate date,--注册时间
+	realname varchar(40),--真实姓名
 	sex int,--性别 0男1女
 	birthday varchar(10),--生日
 );
 
-select * from users;
+
 --关注
 create table concern(
 	id int primary key ,
@@ -31,7 +32,7 @@ create table blog(
 	uid int,--发文者 id
 	text varchar(300),--博文内容
 	pic varchar(1000),--附加图片
-	video
+	video varchar(1000),
 	fdate date,--发文时间
 	source bigint--是否为转发 如果为转发 则存来源微博id
 )
@@ -47,6 +48,14 @@ create table groups(
 	id int primary key,
 	name varchar(30) --分组名
 )
+insert into groups(id,name) values(0,'朋友');
+insert into groups(id,name) values(1,'同事');
+insert into groups(id,name) values(2,'亲人');
+insert into groups(id,name) values(3,'搞笑');
+insert into groups(id,name) values(4,'名人明星');
+
+select name from groups limit 0,4
+
 --用户分组表
 create table user_group(
 	id  int primary key,	
